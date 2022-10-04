82ac7e5936c8e5ee66d5447428fa49506053fbd0
GORINI Marco <marco.gorini@soprasteria.com>
2022-10-04 12:28:44 +0200

    Seconda modifica

--- src/com/soprasteria/Saluto.java
+++ src/com/soprasteria/Saluto.java
@@ -3,6 +3,6 @@ package com.soprasteria;
 public class Saluto {
 	
 	public static void main(String[] args) {
-		System.out.println("Benvenuto.");
+		System.out.println("Benvenuto modificato.");
 	}
 }
